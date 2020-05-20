ee98474621cb6655c6ab2c861ccfe8485fc258eb
Lucio Benini <iiriya@iiriya.com>
2020-05-20 17:54:13 +0200

    Fix: Blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,8 @@
 const path = require(`path`)
-const slugify = require(`slug`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const posts = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 2000
-          filter: {
-            fields: {
-              slug: {
-                regex: "/^\/(blog)\//s"
-              }
-            }
-          }
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-                tags
-                categories
-              }
-              frontmatter {
-                title
-                tags
-                categories
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-
-  if (posts.errors) {
-    throw posts.errors
-  }
-
+  const postsPerPage = 15
   const remarks = await graphql(
     `
       {
@@ -73,23 +35,6 @@ exports.createPages = async ({ graphql, actions }) => {
   if (remarks.errors) {
     throw remarks.errors
   }
-
-  const postsPerPage = 15
-  const numPages = Math.ceil(posts.data.allMarkdownRemark.edges.length / postsPerPage)
-
-  Array.from({ length: numPages }).forEach((_, i) => {
-    createPage({
-      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-      component: path.resolve("./src/templates/blog-list-template.js"),
-      context: {
-        limit: postsPerPage,
-        skip: i * postsPerPage,
-        pages: numPages,
-        current: i + 1,
-        slug: "blog"
-      }
-    })
-  })
 
   const pages = []
 
