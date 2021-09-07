9c78d682d6f905134955fdfd581ba0f92034543f
Lucio Benini <iiriya@iiriya.com>
2021-09-07 17:10:57 +0200

    Fix: pagination

--- src/components/ui/article/page-grid.jsx
+++ src/components/ui/article/page-grid.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import { groupify } from "@pittica/gatsby-plugin-utils"
 
 import ArticleCage from "./article-cage"
@@ -8,20 +7,11 @@ import Title from "../title"
 import "../../../scss/ui/article/_page-grid.scss"
 
 export default function PageGrid({ node, group }) {
-  const title = node.title || node.slug
   const slug = groupify(node.slug, group)
 
   return (
     <ArticleCage node={node} slug={slug} featured={false} className="page-grid">
-      <Title link={slug}>{title}</Title>
-      <section>
-        <Link
-          to={slug}
-          dangerouslySetInnerHTML={{
-            __html: node.description || node.subtitle || node.excerpt,
-          }}
-        />
-      </section>
+      <Title link={slug}>{node.title || node.slug}</Title>
     </ArticleCage>
   )
 }
